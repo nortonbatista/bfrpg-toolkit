f4a64b862063d85f7013dc3a16baf14e05199b34
Norton Batista Abdala <49415914+nortonbatista@users.noreply.github.com>
2024-07-03 20:49:12 -0300

    Create methods to handle users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  private
+
+  def current_user = Current.user ||= authenticate_user_from_session
+  helper_method :current_user
+
+  def authenticate_user_from_session = User.find_by(id: session[:user_id])
+
+  def user_signed_in? = current_user.present?
+  helper_method :user_signed_in?
+
+  def login(user)
+    Current.user = user
+
+    reset_session
+    session[:user_id] = user.id
+  end
 end
