abcc2a776e083695ef6cbc594eb501e31572789e
Norton Batista Abdala <49415914+nortonbatista@users.noreply.github.com>
2024-07-03 21:28:30 -0300

    Create method for user to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,26 @@
 class SessionsController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    user = User.authenticate_by sessions_params
+
+    if user
+      login user
+      redirect_to root_path, notice: 'Logged in successfully'
+    else
+      flash[:alert] = 'Invalid email or password'
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def destroy
     logout
     redirect_to root_path, notice: 'Logged out successfully'
+  end
+
+  private
+
+  def sessions_params
+    params.permit(:email, :password)
   end
 end
