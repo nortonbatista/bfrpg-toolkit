9d14f652a7363ae7b83b04c14b33ee3eebd0c6eb
Norton Batista Abdala <49415914+nortonbatista@users.noreply.github.com>
2024-07-07 19:59:55 +0000

    Encrypt user email (#4)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe User do
+  it { is_expected.to have_secure_password }
+
   describe '#email' do
     subject { create(:user) }
 
+    it { is_expected.to encrypt(:email).deterministic(true) }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
     it { is_expected.to normalize(:email).from(' FoO@bAr.COM ').to('foo@bar.com') }
