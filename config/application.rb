ea6e1c9988d94b18969e3153a6b87dfba3629f97
Norton Batista Abdala <49415914+nortonbatista@users.noreply.github.com>
2024-07-14 20:13:58 +0000

    Change db encryption config from Rails credentias to .env (#13)

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module BfrpgToolkit
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Configuration for db:encryption
+    config.active_record.encryption.primary_key = ENV['ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY']
+    config.active_record.encryption.deterministic_key = ENV['ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY']
+    config.active_record.encryption.key_derivation_salt = ENV['ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT']
   end
 end
